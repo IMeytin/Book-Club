8d10f14293f78f3c22a3242813452c729b8d4f1c
Imeytin <irynameytin@gmail.com>
2023-10-13 12:24:16 -0700

    Added Style and JSX

--- src/Books.js
+++ src/Books.js
@@ -1,7 +1,22 @@
+import bookImg from "./Style-Images/BookImg.png"
 function Books() {
+    
     return ( 
         <main>
-            <h2>Find Your Next Literary Adventure</h2>
+            <div className="book-section-header">
+                <img src={bookImg} alt="book" width={100}/>
+                <h2 className="header">Find Your Next Literary Adventure</h2>
+                <img src={bookImg} alt="book" width={100}/>
+            </div>
+            <form>
+                <input className="search-input" type="text" />
+                <button className="btn"><span className="material-icons-outlined">search</span></button>
+            </form>
+
+
+            <section>
+                <h2>BOOK list</h2>
+            </section>
         </main>
     );
 }
